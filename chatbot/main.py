c59171980c5842e3f0ac88d39220ea3c545d0b4a
pedroid-js <pedrofcaffarena@gmail.com>
2021-03-24 02:27:12 +0100

    c

--- chatbot/main.py
+++ chatbot/main.py
@@ -124,18 +124,9 @@ class BotAPI(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length) 
  
         json_string = json.loads(post_data.decode('utf-8'))
         response = json.dumps(Bot(json_string)).encode('utf-8')
