d901eaaaca0b0faca56a6fa5b075c0bc01ddac01
pedroid-js <pedrofcaffarena@gmail.com>
2021-03-24 01:41:31 +0100

    .

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const utf8 = require('utf8')
 const WhatsApp = require('./whatsapp/functions')
 const { post_msg } = require('./assets/js/utils')
+const log = require('simple-node-logger').createSimpleLogger();
 
 function main() {
   const whatsapp = new WhatsApp()
@@ -10,21 +11,26 @@ function main() {
   whatsapp.onQR()
   whatsapp.onReady().then(() => console.log("Client is ready"))
   whatsapp.onMessage(async msg => {
-    console.log("MESSAGE FROM", msg.from)
-    if (allowed.includes(msg.from)) {
-      console.log(msg.body)
+    const { from, body } = msg
+    if (allowed.includes(from)) {
       let answ = ""
       try {
-        const { reply } = await post_msg({ msg: msg.body })
+        const { reply, prob } = await post_msg({ msg: body })
         try {
           answ = utf8.decode(reply)
         } catch (e) {
           answ = reply
         }
+        log.info(
+          " FROM: ", from,
+          " BODY: ", body,
+          " REPLY: ", reply,
+          " PROB: ", prob
+        )
       } catch (e) {
         console.error(e)
       }
-      whatsapp.send(msg.from, answ)
+      whatsapp.send(from, answ)
     }
   })
   whatsapp.init()
